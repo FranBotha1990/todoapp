279a429c9a360d4b942450b9f4e3faa7e402033b
FranBotha1990 <f.botha@outlook.com>
2024-08-23 21:31:10 +0300

    Correct root directory and relative paths to applications and routes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-import models
-from database import engine
-from routers import auth, todos, admin, users
+from .models import Base
+from .database import engine
+from .routers import auth, todos, admin, users
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind = engine)
+Base.metadata.create_all(bind = engine)
 
 # System health check
 @app.get("/healthy")
@@ -17,4 +17,5 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
 
