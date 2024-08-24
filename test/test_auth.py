61e52c3514c23c8d0cc92d44c82a22bc3665f004
FranBotha1990 <f.botha@outlook.com>
2024-08-24 13:09:45 +0300

    Add create access token test to auth test

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,6 +1,8 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
 from fastapi import status
+from jose import jwt
+from datetime import timedelta
 
 
 # Override dependency functions to ensure testing environment and session database is used
@@ -21,3 +23,17 @@ def test_authenticate_user(test_user):
     assert wrong_password_user is False
 
 
+# Test creating access token
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'User'
+    expires_delta = timedelta(days = 1)
+
+    token =  create_access_token(username, user_id, role, expires_delta)
+
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM], options = {'verify_signature': False})
+
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
