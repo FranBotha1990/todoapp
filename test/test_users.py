f4a600719aba3a859026edf50bdad9159359d311
FranBotha1990 <f.botha@outlook.com>
2024-08-24 12:50:00 +0300

    Add user unit tests for testing password change success and failure, and changing phone number

--- test/test_users.py
+++ test/test_users.py
@@ -18,3 +18,24 @@ def test_return_user(test_user):
     assert response.json()['last_name'] == 'Roby'
     assert response.json()['role'] == 'Admin'
     assert response.json()['phone_number'] == '111 111 1111'
+
+
+# Test changing user password successfully
+def test_change_password_success(test_user):
+    response = client.put("/user/password", json = {"password": "testpassword", 
+                                                    "new_password": "newpassword"})
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+# Test unsuccessfully chaning user password
+def test_change_password_fail(test_user):
+    response = client.put("/user/password", json = {"password": "wrong_password", 
+                                                    "new_password": "newpassword"})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail': 'Error on password change.'}
+
+
+# Test chaning phone number
+def test_change_phonenumber_success(test_user):
+    response = client.put("/user/phonenumber/2222222222")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
