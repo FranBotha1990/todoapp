4fac2560263acf7f98abfe7167a2f0bfa867f59b
FranBotha1990 <f.botha@outlook.com>
2024-08-24 13:18:08 +0300

    Create auth test for creating access token and getting current user valid token; import pytest-asyncio

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,8 +1,9 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
 from fastapi import status
 from jose import jwt
 from datetime import timedelta
+import pytest
 
 
 # Override dependency functions to ensure testing environment and session database is used
@@ -37,3 +38,13 @@ def test_create_access_token():
     assert decoded_token['sub'] == username
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
+
+
+# Test getting the current user valid token
+@pytest.mark.asyncio
+async def test_get_current_user_valid_tokens():
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'Admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm = ALGORITHM)
+
+    user = await get_current_user(token = token)
+    assert user == {'username': 'testuser', 'id': 1, 'user_role': 'Admin'}
